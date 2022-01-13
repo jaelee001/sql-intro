808a9fa871e58e172755a9b8bbcee77b81c56558
jaelee001 <jaehee.lee@kellogg.northwestern.edu>
2022-01-13 03:07:22 +0000

    lab_4

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -5,4 +5,4 @@
 
 SELECT players.first_name, players.last_name, SUM(stats.hits)
 FROM stats INNER JOIN players ON players.id=stats.player_id
-WHERE first_name = "Barry" AND last_name = "Bonds";
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds";
